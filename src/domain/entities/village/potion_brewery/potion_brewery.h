250e6c6fc2ddd29b9facfada6b636cd9d8176d0a
Maris Shardyko <marrttaoburberry@gmail.com>
2025-04-01 04:57:32 +0300

    just created class potion brewery now im gonna sleep and tomorrow i will try finish that and refactor providers also me wanna try sfml framework

---
+++ src/domain/entities/village/potion_brewery/potion_brewery.h
@@ -0,0 +1,10 @@
+#ifndef POTION_BREWERY_H
+#define POTION_BREWERY_H
+#include "../npc/potion_maker.h"
+class PotionBrewery {
+public:
+	PotionMaker potionMaker;
+	
+};
+
+#endif // !POTION_BREWERY_H
